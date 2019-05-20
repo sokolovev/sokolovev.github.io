8588f981707d25a49cbf0453f993e0e7aa08bb45
sokolovev <46241815+sokolovev@users.noreply.github.com>
2019-05-20 14:25:57 +0600

    Add files via upload

---
+++ top5/js/script.js
@@ -0,0 +1,40 @@
+$(document).ready(function () {
+  var clock;
+  clock = $(".clock").FlipClock({
+    clockFace : "DailyCounter",
+    language : "ru",
+    autoStart : false,
+    callbacks : {
+      stop : function() {
+        $('.massage').html('Время прошло');
+      }
+    }
+  });
+
+  // var dt = "May 21 2019 20:22:48";
+  // var first = new Date(dt);
+  // var last = Date.now();
+  // var remaining = first - last;
+  // remaining /=1000;
+
+  clock.setTime(555400);
+  clock.setCountdown(true);
+  clock.start(); 
+
+  var btn = $('.content-text__button'),
+      modal = $('.wrapper'),
+      modalb = $('.modal')
+
+  btn.click(function () { 
+    modal.addClass('active');
+  });
+  modalb.click(function (event) { 
+    event.stopPropagation();
+  });
+  modal.click(function () { 
+    modal.removeClass('active');
+  });
+
+
+});
+
