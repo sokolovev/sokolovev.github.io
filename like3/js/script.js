4b164307fbc1afa613542d385c9fd1faa42caaa1
sokolovev <46241815+sokolovev@users.noreply.github.com>
2019-06-04 19:16:42 +0600

    Add files via upload

---
+++ like3/js/script.js
@@ -0,0 +1,45 @@
+$(document).ready(function(){
+  $('.test').slick({
+    slidesToShow: 3,
+    infinite: true,
+    speed: 300,
+    arrow: true,
+    prevArrow: '<div class="slider-arrow-left"><img src="img/Arrow-left.png"></div>',
+    nextArrow: '<div class="slider-arrow-right"><img src="img/Arrow-right.png"></div>',
+    responsive: [
+      {
+        breakpoint: 1324,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true,
+        }
+      },
+      {
+        breakpoint: 1000,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
+
+});
+
+
+$(".buy-menu__btn1").click(function () { 
+  $(".buy-like").removeClass("none");
+  $(".buy-massage").addClass("none");
+  $(".buy-menu__line1").removeClass("none");
+  $(".buy-menu__line2").addClass("none");
+});
+$(".buy-menu__btn2").click(function () { 
+  $(".buy-massage").removeClass("none");
+  $(".buy-like").addClass("none");
+  $(".buy-menu__line2").removeClass("none");
+  $(".buy-menu__line1").addClass("none");
+});
