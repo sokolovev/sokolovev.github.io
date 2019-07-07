e3ed55952a5fa8de097d5397864107990fd3335d
sokolovev <46241815+sokolovev@users.noreply.github.com>
2019-07-07 23:16:43 +0600

    Add files via upload

---
+++ project/js/scripts.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+  $('#fullpage').fullpage({
+  anchors:['firstSection', 'secondSection', 'thirdSection'],
+  // menu: '.main-nav ul',
+  });
+
+  $('#menuBtn').click(function () { 
+    $('#mainNav').show();
+  });
+  $('#mainNav a').click(function () { 
+    $('#mainNav').hide();
+  });
+  $('.popup-close').click(function () { 
+    $('.block3-popup-wrapper').hide();
+  });
+  $('#popupFade1').click(function () { 
+    $('#popUp1').show();
+  });
+  $('#popupFade2').click(function () { 
+    $('#popUp2').show();
+  });
+  
+  $('#popupFade3').click(function () { 
+    $('#popUp3').show();
+  });
+  
+  $('#popupFade4').click(function () { 
+    $('#popUp4').show();
+  });
+  
+});
